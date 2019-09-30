2a351fcecdde3c380891f2c34609c389fd9550a4
Sohel <saa@one.com>
2019-09-30 19:23:40 +0400

    Seperate game engine logic

---
+++ server/GameEngine.js
@@ -0,0 +1,124 @@
+const uuid = require('uuid/v1');
+
+class GameEngine {
+    constructor ({io, maxPlayers = 2, maxStartingNumber = 100, divideBy = 3}) {
+        this.io = io;
+        this.maxPlayers = maxPlayers;
+        this.maxStartingNumber = maxStartingNumber;
+        this.divideBy = divideBy;
+        this.players = [];
+        this.startingNumber = null;
+        this.lastMoveData = null;
+    }
+
+    start () {
+        this.io.on('connection', (socket) => {
+            socket.on('JOIN_GAME', (data) => {
+                this.onJoinGame(socket, data)
+            });
+            socket.on('ADD_MOVE', (data) => {
+                this.onAddGame(socket, data);
+            });
+            socket.on('disconnect', () => {
+                this.onDisconnect(socket);
+            });
+        });
+    }
+
+    onJoinGame(socket, data) {
+        if (this.players.length >= this.maxPlayers) {
+            socket.emit('ROOM_FULL');
+            return;
+        }
+        const playerInfo = {
+            name: data.playerName,
+            id: uuid(),
+            socketID: socket.id
+        };
+        socket.emit('ENTERED_GAME', {
+            name: playerInfo.name,
+            id: playerInfo.id
+        });
+        this.players.push(playerInfo);
+
+        console.log(playerInfo.name + ' has connected to the game');
+        console.log('Connected players: ', this.players.map(player => player.name));
+
+        if (this.players.length === this.maxPlayers) {
+            this.startingNumber = this.generateStartingNumber();
+            this.io.emit('GAME_STARTED', {
+                startingNumber: this.startingNumber,
+                startingPlayer: this.players[0].id
+            });
+
+            console.log('game has started between: ', this.players.map(player => player.name));
+        }
+    }
+
+    onAddGame(socket, data) {
+        //if same player sending move more than once in a row, reject it
+        if (this.lastMoveData && socket.id === this.lastMoveData.socketID) {
+            return;
+        }
+        const {equation, output, gameLost} = this.getResult(data.input);
+        if (!gameLost) {
+            const moveData = {
+                playerName: data.playerName,
+                playerID: data.playerID,
+                input: data.input,
+                equation,
+                output
+            };
+            this.io.emit('ADD_MOVE', moveData);
+            this.lastMoveData = {
+                ...moveData,
+                socketID: socket.id
+            };
+            if (output === 1) {
+                socket.emit('GAME_WON', {
+                    type: 'GOAL_REACHED',
+                    message: ''
+                });
+                socket.broadcast.emit('GAME_LOST', {
+                    type: 'OPPONENT_WON',
+                    message: `${data.playerName} won the game`
+                });
+            }
+        } else {
+            socket.emit('GAME_LOST', gameLost);
+            socket.broadcast.emit('GAME_WON', {
+                type: 'WRONG_INPUT_BY_OPPONENT',
+                message: `${data.playerName} entered a wrong input`
+            });
+        }
+    }
+
+    onDisconnect(socket) {
+        this.players = this.players.filter((player) => {
+            return player.socketID !== socket.id;
+        });
+        this.lastMoveData = null;
+        this.io.emit('OPPONENT_DISCONNECTED');
+        console.log('Disconnected. Remaining players: ', this.players.map(player => player.name));
+    }
+
+    generateStartingNumber() {
+        return Math.round((Math.random() * (this.maxStartingNumber - this.divideBy)) + this.divideBy);
+    }
+
+    getResult(input) {
+        const lastOuput = this.lastMoveData ? this.lastMoveData.output : this.startingNumber;
+        const output = (input + lastOuput) / 3;
+        const equation = `[(${input} + ${lastOuput}) / ${this.divideBy}] = ${output}`;
+        let gameLost = null;
+        if ((input+lastOuput) % 3 !== 0) {
+            gameLost = {
+                type: 'WRONG_INPUT',
+                message: `${input + lastOuput} is not divisible by ${this.divideBy}`
+            };
+        }
+        return {output, equation, gameLost};
+    }
+}
+
+module.exports = GameEngine;
--- server/index.js
+++ server/index.js
@@ -2,120 +2,22 @@ const express = require('express');
 const path = require('path');
 const httpModule = require('http');
 const socketIO = require('socket.io');
-const uuid = require('uuid/v1');
+const GameEngine = require('./GameEngine');
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
-const MAIN_JS = path.join(DIST_DIR, 'main.js');
 
 const app = express();
 const http = httpModule.createServer(app);
 const io = socketIO(http);
-
-const MAX_PLAYERS = 2;
-const MAX_STARTING_NUMBER = 100;
-const DIVIDE_BY = 3;
-let players = [];
-let startingNumber;
-let lastMoveData;
 
 app.use(express.static(DIST_DIR));
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
 });
 
-io.on('connection', (socket) => {
-    socket.on('JOIN_GAME', (data) => {
-        if (players.length >= MAX_PLAYERS) {
-            socket.emit('ROOM_FULL');
-            return;
-        }
-        const playerInfo = {
-            name: data.playerName,
-            id: uuid(),
-            socketID: socket.id
-        };
-        socket.emit('ENTERED_GAME', {
-            name: playerInfo.name,
-            id: playerInfo.id
-        });
-        players.push(playerInfo);
-
-        console.log(playerInfo.name + ' has connected to the game');
-        console.log('Connected players: ', players.map(player => player.name));
-
-        if (players.length === MAX_PLAYERS) {
-            startingNumber = Math.round((Math.random() * (MAX_STARTING_NUMBER - DIVIDE_BY)) + DIVIDE_BY);
-            io.emit('GAME_STARTED', {
-                startingNumber,
-                startingPlayer: players[0].id
-            });
-
-            console.log('game has started between: ', players.map(player => player.name));
-        }
-    });
-
-    socket.on('ADD_MOVE', (data) => {
-        //if same player sending move more than once in a row, reject it
-        if (lastMoveData && socket.id === lastMoveData.socketID) {
-            return;
-        }
-        const {equation, output, gameLost} = getResult(data.input);
-        if (!gameLost) {
-            const moveData = {
-                playerName: data.playerName,
-                playerID: data.playerID,
-                input: data.input,
-                equation,
-                output
-            };
-            io.emit('ADD_MOVE', moveData);
-            lastMoveData = {
-                ...moveData,
-                socketID: socket.id
-            };
-            if (output === 1) {
-                socket.emit('GAME_WON', {
-                    type: 'GOAL_REACHED',
-                    message: ''
-                });
-                socket.broadcast.emit('GAME_LOST', {
-                    type: 'OPPONENT_WON',
-                    message: `${data.playerName} won the game`
-                });
-            }
-        } else {
-            socket.emit('GAME_LOST', gameLost);
-            socket.broadcast.emit('GAME_WON', {
-                type: 'WRONG_INPUT_BY_OPPONENT',
-                message: `${data.playerName} entered a wrong input`
-            });
-        }
-    });
-
-    socket.on('disconnect', () => {
-        players = players.filter((player) => {
-            return player.socketID !== socket.id;
-        });
-        lastMoveData = null;
-        io.emit('OPPONENT_DISCONNECTED');
-        console.log('Disconnected. Remaining players: ', players.map(player => player.name))
-    });
-});
-
-function getResult (input) {
-    const lastOuput = lastMoveData ? lastMoveData.output : startingNumber;
-    const output = (input + lastOuput) / 3;
-    const equation = `[(${input} + ${lastOuput}) / ${DIVIDE_BY}] = ${output}`;
-    let gameLost = null;
-    if ((input+lastOuput) % 3 !== 0) {
-        gameLost = {
-            type: 'WRONG_INPUT',
-            message: `${input + lastOuput} is not divisible by ${DIVIDE_BY}`
-        };
-    }
-    return {output, equation, gameLost};
-}
+const gameEngine = new GameEngine({io});
+gameEngine.start();
 
 http.listen(3000, () => {
     console.log('Listening on port 3000');
